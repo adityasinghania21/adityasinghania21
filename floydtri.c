0a772ab84edc7148f063a88110363c0db7f9a9cb
adityasinghania21 <75612020+adityasinghania21@users.noreply.github.com>
2024-12-24 19:31:32 +0530

    Update floydtri.c

--- floydtri.c
+++ floydtri.c
@@ -9,9 +9,10 @@ int main() {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             printf("%d ", num);
-            num++;
+            
         }
         printf("\n");
+        num++;
     }
 
     return 0;
